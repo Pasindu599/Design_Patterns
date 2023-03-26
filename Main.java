c879928e37f1b264ef7f78fffc8aa736c1ff0f88
Pasindu599 <pasindusankalpa599@gmail.com>
2023-03-27 02:56:24 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] arg){
+        Singleton single1 = Singleton.getInstance();
+        Singleton single2 = Singleton.getInstance();
+
+    }
+}
